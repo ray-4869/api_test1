7eb06091424842e79ae575d2248f92d5481c13f7
ray-4869 <rlfgustn777@gmail.com>
2024-11-06 10:04:08 +0900

    start

---
+++ src/main/java/com/example/demo/article/entity/Article.java
@@ -0,0 +1,15 @@
+package com.example.demo.article.entity;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class Article {
+}
