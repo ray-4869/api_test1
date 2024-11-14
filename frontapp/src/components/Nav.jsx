1316495b0d6e915710278c3b4aba21be339e9ef4
ray-4869 <rlfgustn777@gmail.com>
2024-11-14 13:51:45 +0900

    logo

--- frontapp/src/components/Nav.jsx
+++ frontapp/src/components/Nav.jsx
@@ -1,18 +1,41 @@
+import React from "react"
 import { Link } from "react-router-dom"
+import "../CSS/NavBar.css" // 네비게이션 스타일 CSS import
+import logo from "../assets/logoimage2.gif" //로고이미지
 
 function Nav() {
   return (
-    <ul>
-      <li>
-        <Link to="/">홈</Link>
-      </li>
-      <li>
-        <Link to="/auth/login">로그인</Link>
-      </li>
-      <li>
-        <Link to="/article/list">게시글 목록</Link>
-      </li>
-    </ul>
+    <div className="top-bar">
+      <div></div>
+      <div className="project-brand">
+        <img src={logo} alt="Logo" className="logo-image" />
+        <div className="project-name">Fiction Place</div>
+      </div>
+
+      {/* 네비게이션 메뉴 */}
+      <nav className="navigation">
+        <ul className="nav-links">
+          <li>
+            <Link to="/">홈</Link>
+          </li>
+          <li>
+            <Link to="/auth/login">로그인</Link>
+          </li>
+          <li>
+            <Link to="/article/list">게시글 목록</Link>
+          </li>
+          <li>
+            <Link to="/myprofil">프로필</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* 검색창 */}
+      <div className="search-box">
+        <input type="text" className="search-input" placeholder="검색..." />
+        <div className="search-icon"></div>
+      </div>
+    </div>
   )
 }
 
